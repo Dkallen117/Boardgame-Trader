640694cc05c62c272920fe0a940cd82eaaf60dff
Buney <dpkinser93@gmail.com>
2021-10-02 11:02:48 -0600

    fields tweaked again, and modified user seeds

--- server/models/User.js
+++ server/models/User.js
@@ -8,16 +8,6 @@ const userSchema = new Schema({
     trim: true,
     unique: true,
   },
-  first_name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   email: {
     type: String,
     required: true,
