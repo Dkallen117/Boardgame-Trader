a8463567fbddcf9f55d3be6abbfd4ba8a0bdc221
Buney <dpkinser93@gmail.com>
2021-10-04 19:06:13 -0600

    added queries on the front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,8 +12,6 @@ export const ADD_PROFILE = gql`
   }
 `;
 
-
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -25,4 +23,3 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-
