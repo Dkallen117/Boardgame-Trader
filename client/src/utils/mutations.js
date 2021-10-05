1566f2702589dbff2ba6267f3a7eb7a8f3c187b7
Buney <dpkinser93@gmail.com>
2021-10-04 19:57:22 -0600

    fixed a user create error

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,11 +2,11 @@ import { gql } from '@apollo/client';
 
 export const ADD_PROFILE = gql`
   mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+    addProfile(username: $name, email: $email, password: $password) {
       token
       profile {
         _id
-        name
+        username
       }
     }
   }
@@ -18,7 +18,7 @@ export const LOGIN_USER = gql`
       token
       profile {
         _id
-        name
+        username
       }
     }
   }
