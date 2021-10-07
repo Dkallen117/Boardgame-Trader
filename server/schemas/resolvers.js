161a115977176a038108e8cc641f0f69e2586f12
Buney <dpkinser93@gmail.com>
2021-10-06 20:46:36 -0600

    removed dev console log

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,7 +27,6 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      console.log('Im hit!!', username, email, password)
       const user = await User.create({ username, email, password });
       const token = signToken(user);
 
