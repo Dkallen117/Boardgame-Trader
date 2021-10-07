ba9d872e8c1117efca0a9e77269106657645d5f9
Daniel Allen <danielkentonallen@gmail.com>
2021-10-06 20:11:45 -0600

    working on home page

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -15,6 +15,8 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+import { Link } from 'react-router-dom';
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -26,7 +28,12 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function ProfileList() {
+const gameList = ({ listing, title }) => {
+  if (!listing.length) {
+    return <h3>No Profiles Yet</h3>;
+  }
+
+export default function GameList() {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -46,14 +53,12 @@ export default function ProfileList() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
+        title={title}
         subheader="September 14, 2016"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {listing.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -74,27 +79,15 @@ export default function ProfileList() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+          <Typography paragraph>Listing Details:</Typography>
+          <Typography >
+           {listing.price}
           </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          <Typography >
+          {listing.quantity}
           </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+          <Typography >
+          {listing.genre}
           </Typography>
           <Typography>
             Set aside off of the heat to let rest for 10 minutes, and then serve.
@@ -105,4 +98,4 @@ export default function ProfileList() {
   );
 };
 
-
+}
