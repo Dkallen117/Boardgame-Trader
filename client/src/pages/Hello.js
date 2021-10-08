f05a424244dfbb215e0b6abde1b07db656e89607
Subashsunar <subashsunar00@gmail.com>
2021-10-07 21:55:33 -0600

    Added a lists of link card

--- client/src/pages/Hello.js
+++ client/src/pages/Hello.js
@@ -1,31 +1,60 @@
 import * as React from "react";
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
-
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Hello = () => {
   return (
     <div>
-      <p>This is profile page...</p>
       <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            User Name
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="User Avatar"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              User Name
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
+
+        <Link href="#" variant="">
+          {"Order / Listing"}
+        </Link>
+        <br />
+        <Link href="#" variant="">
+          {"Saved Items"}
+        </Link>
+        <br />
+        <Link href="#" variant="">
+          {"Message"}
+        </Link>
+        <br />
+        <Link href="#" variant="">
+          {"Purchased"}
+        </Link>
+        <br />
+        <Link href="#" variant="">
+          {"Settings"}
+        </Link>
+        <br />
+        <CardActions>
+          <Button size="small">Log Out</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
