e91543c759ab430aa79448ff20a7e363094bd677
Buney <dpkinser93@gmail.com>
2021-10-10 21:16:22 -0600

    users can load their listings from the profile

--- client/src/pages/OrderList.js
+++ client/src/pages/OrderList.js
@@ -1,7 +1,34 @@
 import * as React from "react";
+import { Button } from "@mui/material";
+import { useQuery } from '@apollo/client';
+
+import GameList from '../components/GameList';
+
+import { QUERY_ME } from '../utils/queries';
 
 const OrderList = () => {
-  return <div>Order list</div>;
+  const { data, loading} = useQuery(QUERY_ME);
+  const listings = data?.me?.listings;
+  console.log(listings)
+  return( 
+    <main>
+      <h1>My Listings</h1>
+      <div>
+       {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            listings.length > 0 ? (
+              <GameList listings={listings}/>
+            ) : (
+              <h4>Looks like nothing is here yet...</h4>
+            )
+          )}
+      </div>
+      <Button variant='contained' href={'/new'}>
+        Create New
+      </Button>      
+    </main>
+  );
 };
 
 export default OrderList;
