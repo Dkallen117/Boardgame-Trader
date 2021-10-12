4441e2c73a670ac3d9222099c76196f597687c57
Daniel Allen <danielkentonallen@gmail.com>
2021-10-11 19:01:23 -0600

    removed google dialog box

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -96,28 +96,7 @@ export default function Login (props)  {
       <Button  href={`/signup`}
         variant='contained'
       >Need an account?</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+      
 </Box>
   );
 };
