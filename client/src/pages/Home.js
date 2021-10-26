8a1a88ffd651ac98e2ab866ff9a74021f5dce3de
Buney <dpkinser93@gmail.com>
2021-10-25 20:17:43 -0600

    removed unused import

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import GameList from '../components/GameList';
 import { QUERY_ALL_LISTINGS } from '../utils/queries';
-import Box from '@mui/material/Box';
 
 const Home = () => {
 const { data, loading } = useQuery(QUERY_ALL_LISTINGS);
