ad1a2a59344194b9bd0b5bbad55503b79e9eebb2
Buney <dpkinser93@gmail.com>
2022-01-12 21:27:16 -0700

    moved sorting functions into a utils file

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -20,6 +20,7 @@ import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import data from '../../utils/data';
+import sorting from '../../utils/sorting';
 
 
 const ExpandMore = styled((props) => {
@@ -43,27 +44,6 @@ const GameList = ({listings})  =>  {
     last: [],
     original: [...listings],
   });
-
-  // Define a function that will be use to sort listings alphabetically
-  const alphaSort = (x, y) => {
-    // Set the titles to uppercase for better comparisons
-    let xTitle = x.title.toUpperCase();
-    let yTitle = y.title.toUpperCase();
-    if(xTitle < yTitle) {return -1;}
-    if(xTitle > yTitle) {return 1;}
-    return 0;
-  }
-
-  // Define a compare function that will sort listings by number
-  const priceSort = (x, y) => {
-    // Set the prices to float
-    let xPrice = parseFloat(x.price);
-    let yPrice = parseFloat(y.price);
-    // Compare
-    if(xPrice < yPrice) {return -1;}
-    if(xPrice > yPrice) {return 1;}
-    return 0;
-  }
 
   const handleListChange = (event) => {
     const selectName = event.target.name;
@@ -99,16 +79,16 @@ const GameList = ({listings})  =>  {
     // Determine how to sort with a switch
     switch(sort) {
       case 'AA':
-        list = list.sort(alphaSort);
+        list = list.sort(sorting.titleSort);
         break;
       case 'AD':
-        list = list.sort(alphaSort).reverse();
+        list = list.sort(sorting.titleSort).reverse();
         break;
       case 'PA':
-        list = list.sort(priceSort);
+        list = list.sort(sorting.priceSort);
         break;
       case 'PD':
-        list = list.sort(priceSort).reverse();
+        list = list.sort(sorting.priceSort).reverse();
         break;
       default:
         // Don't sort if nothing selected
