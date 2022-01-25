071185aeb01726f1208a200e6be6b0f5827f659c
Buney <dpkinser93@gmail.com>
2022-01-24 19:29:29 -0700

    fixed profile page errors

--- client/src/pages/Hello.js
+++ client/src/pages/Hello.js
@@ -22,91 +22,92 @@ const Hello = () => {
         <h1>Loading...</h1>
       ) : (
         <Grid container spacing={3} style={{ marginTop: "9px" }}>
-        <Grid xs={4}>
-          <Paper
-            sx={{
-              maxWidth: 400,
-              height: 125,
-              my: 1,
-              mx: "auto",
-              p: 2,
-              backgroundColor: "#C9D1D7",
-              color: "#437A85",
-            }}
-          >
-            <Grid container wrap="nowrap" spacing={0}>
-              <Grid item>
-                <Avatar>{data?.me?.username[0]}</Avatar>
+
+          <Grid item xs={4}>
+            <Paper
+              sx={{
+                maxWidth: 400,
+                height: 125,
+                my: 1,
+                mx: "auto",
+                p: 2,
+                backgroundColor: "#C9D1D7",
+                color: "#437A85",
+              }}
+            >
+              <Grid container>
+                <Grid item>
+                  <Avatar>{data?.me?.username[0]}</Avatar>
+                </Grid>
+                <Grid item>
+                  <Typography
+                    style={{ paddingLeft: "0.5625rem", marginTop: "0.4375rem" }}
+                  >
+                    {data?.me?.username}
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
+            </Paper>
+
+            <Card
+              style={{ lineHeight: "2" }}
+              sx={{
+                maxWidth: 400,
+                height: 215,
+                my: 1,
+                mx: "auto",
+                p: 2,
+                backgroundColor: "#C9D1D7",
+                color: "#437A85",
+              }}
+              spacing={{ xs: 2, md: 3 }}
+              columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+              <a href="/OrderList" onClick={() => this.toggModal("Whatever")}>
+                My Listings
+              </a>
+              <br />
+              <a href="/favorites" onClick={() => this.toggModal("Whatever")}>
+                Saved Listings
+              </a>
+              <br />
+              <a href="/setting" onClick={() => this.toggModal("Whatever")}>
+                Setting
+              </a>
+              <hr style={{ marginTop: "20px" }} />
+
+              <CardActions>
+                <Button size="small" onClick={Auth.logout}>Log Out</Button>
+              </CardActions>
+
+            </Card>
+          </Grid>
+
+          <Grid item xs={7} style={{ marginLeft: "7px" }}>
+            <Card
+              sx={{
+                maxWidth: "100%",
+                height: 348,
+                my: 1,
+                mx: "auto",
+                p: 2,
+                backgroundColor: "#FDFDFD",
+                color: "#437A85",
+              }}
+            >
+              <CardContent>
                 <Typography
-                  style={{ paddingLeft: "0.5625rem", marginTop: "0.4375rem" }}
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
                 >
-                  {data?.me?.username}
+                  {data?.me?.username}'s Profile
                 </Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-
-          <Card
-            style={{ lineHeight: "2" }}
-            sx={{
-              maxWidth: 400,
-              height: 215,
-              my: 1,
-              mx: "auto",
-              p: 2,
-              backgroundColor: "#C9D1D7",
-              color: "#437A85",
-            }}
-            container
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-            <a href="/OrderList" onClick={() => this.toggModal("Whatever")}>
-              My Listings
-            </a>
-            <br />
-            {/* <a href="/message" onClick={() => this.toggModal("Whatever")}>
-              Message
-            </a>
-            <br /> */}
-            <a href="/favorites" onClick={() => this.toggModal("Whatever")}>
-              Saved Listings
-            </a>
-            <br />
-            <a href="/setting" onClick={() => this.toggModal("Whatever")}>
-              Setting
-            </a>
-            <hr style={{ marginTop: "20px" }} />
-            <CardActions>
-              <Button size="small" onClick={Auth.logout}>Log Out</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-        <Grid xs={7} style={{ marginLeft: "7px" }}>
-          <Card
-            sx={{
-              maxWidth: "100%",
-              height: 348,
-              my: 1,
-              mx: "auto",
-              p: 2,
-              backgroundColor: "#FDFDFD",
-              color: "#437A85",
-            }}
-          >
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                {data?.me?.username}'s Profile
+                  
                 <hr style={{ marginTop: "20px" }} />
-                <Grid container>
-                  <Grid xs={7} spacing={2}>
+
+                <Grid container spacing={2}>
+                  <Grid item xs={7}>
                     <Card style={{ paddingTop: "7px", height: "150px" }}>
                       <label
                         style={{
@@ -132,7 +133,8 @@ const Hello = () => {
                       </CardActions>
                     </Card>
                   </Grid>
-                  <Grid xs={3} style={{ marginLeft: "7px" }}>
+
+                  <Grid item xs={3} style={{ marginLeft: "7px" }}>
                     <Card style={{ width: "263px" }}>
                       <CardMedia
                         component="img"
@@ -143,14 +145,16 @@ const Hello = () => {
                     </Card>
                   </Grid>
                 </Grid>
-              </Typography>
-            </CardContent>
-            <CardActions style={{ marginTop: "29px" }}>
-              <Button size="small">Edit Profile</Button>
-            </CardActions>
-          </Card>
+              </CardContent>
+
+              <CardActions style={{ marginTop: "29px" }}>
+                <Button size="small">Edit Profile</Button>
+              </CardActions>
+
+            </Card>
+          </Grid>
+
         </Grid>
-      </Grid>
       )}
       
     </div>
