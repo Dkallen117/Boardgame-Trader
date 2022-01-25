127cd3ab6258f5927dc636ac115ed8c066a4cb30
Buney <dpkinser93@gmail.com>
2022-01-24 19:41:42 -0700

    removed unused imports

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import { useQuery } from '@apollo/client';
-import { QUERY_SINGLE_LISTING } from '../../utils/queries';
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
